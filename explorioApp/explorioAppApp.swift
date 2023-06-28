69f7e7ee13f44b2c5f686ebd4c71b1a30f419db7
Sreenitya Yalamanchili <sreenitya1@outlook.com>
2023-06-28 14:27:52 -0400

    Initial Commit

---
+++ explorioApp/explorioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  explorioAppApp.swift
+//  explorioApp
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct explorioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
