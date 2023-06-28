d0d17bbd6dfe2a387097421516c7945262629b7c
Sreenitya Yalamanchili <sreenitya1@outlook.com>
2023-06-28 14:39:23 -0400

    slayy

--- explorioApp/LocationLanding.swift
+++ explorioApp/LocationLanding.swift
@@ -1,15 +1,74 @@
-//
-//  LocationLanding.swift
-//  explorioApp
-//
-//  Created by Scholar on 6/28/23.
-//
-
 import SwiftUI
 
 struct LocationLanding: View {
+    struct GreenButtonStyle: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .accentColor(.clear)
+                .frame(alignment: .center)
+                .padding()
+                .background(Color(red: 0.38, green: 0.42, blue: 0.22))
+                .cornerRadius(15)
+                .contentShape(Rectangle())
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.green, lineWidth: 0)
+                )
+        }
+    }
+    
+    @State private var expanded = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            Text("**Welcome to**")
+                .font(.largeTitle)
+                .foregroundColor(Color(red: 0.375, green: 0.425, blue: 0.217))
+                .padding()
+            Text("Mumbai, India")
+                .font(.title)
+                .foregroundColor(Color(red: 0.375, green: 0.425, blue: 0.217))
+            
+            Divider()
+                .frame(width: 300, height: 1.5)
+                .overlay(Color(red: 0.375, green: 0.425, blue: 0.217))
+            Spacer()
+                .frame(height: 30)
+            
+            DisclosureGroup("Things To Do") {
+                    Text("Content")
+                    .multilineTextAlignment(.center)
+            }
+            .buttonStyle(GreenButtonStyle())
+            .padding(EdgeInsets(top: 10, leading: 60, bottom: 16, trailing: 60))
+            
+            DisclosureGroup("Weather") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+            }
+            .buttonStyle(GreenButtonStyle())
+            .padding(EdgeInsets(top: 10, leading: 60, bottom: 16, trailing: 60))
+            
+            
+            DisclosureGroup("Food") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+            }
+            .buttonStyle(GreenButtonStyle())
+            .padding(EdgeInsets(top: 10, leading: 60, bottom: 16, trailing: 60))
+            
+            DisclosureGroup("Stay") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+            }
+            .buttonStyle(GreenButtonStyle())
+            .padding(EdgeInsets(top: 10, leading: 60, bottom: 16, trailing: 60))
+            
+            DisclosureGroup("Transportation") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+            }
+            .buttonStyle(GreenButtonStyle())
+            .padding(EdgeInsets(top: 10, leading: 60, bottom: 16, trailing: 60))
+        }
+        
     }
 }
 
--- explorioApp/login page.swift
+++ explorioApp/login page.swift
@@ -1,98 +1,92 @@
-//
-//  login page.swift
-//  explorio
-//
-//  Created by scholar on 6/27/23.
-//hi
-
+/////https://accounts.google.com/v3/signin/identifier?dsh=S2097275425%3A1687976022955142&ifkv=AeDOFXgsKQHRm1o0O6q8qlI3HW54GoSqbobwMGaQ7T6U7YeWWOSF4Zk5bqlQbEMKn6-91tATouEYog&flowName=GlifWebSignIn&flowEntry=ServiceLogin
 import SwiftUI
 struct login_page: View {
-    var body: some View {
-        @State var showDetails = true
-        NavigationStack{
-            ZStack(alignment: .topLeading){
-                VStack (alignment: .leading){
-                      
-                    VStack(){
-                    Image("star")
-                        .padding(.bottom, 75.0)
-                    Button("Login"){
-                    }//end of login button
-                    .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
-                    .cornerRadius(10)
-                    .background(Color.gray)
-                    .foregroundColor(.white)
-                    
-                    
-                    Button("Sign Up"){
-                        }//end of sign up button
-                    .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
-                    .cornerRadius(10)
-                    .background(Color.gray)
-                    .foregroundColor(.white)
-                        
-                        
-                        
-        NavigationLink(destination: homePage()) {
-            Text("Continue as Guest")}
-        .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
-        .cornerRadius(10)
-        .background(Color.gray)
-        .foregroundColor(.white)
-    }
-            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                        }
-                        
-                    }
-                    }
-                }
+  @State private var showSheet = false
+  @State private var showSheet2 = false
+  var body: some View {
+    NavigationStack{
+        VStack (alignment: .center){
+          Image("star")
+            .padding(.bottom, 45.0)
+          VStack(alignment: .center){
+            Button("Login"){
+              self.showSheet = true
+            }//end of login button
+              .frame(width: 250, height: 55)
+              .background(Color(red: 0.74, green: 0.42, blue: 0.15))
+              .cornerRadius(15)
+              .foregroundColor(Color(red: 1, green: 0.98, blue: 0.88))
+              .shadow(radius: 10)
+              .padding(2)
+            Button("Sign Up"){
+              self.showSheet2 = true
+            }//end of sign up button
+              .frame(width: 250, height: 55)
+              .background(Color(red: 0.74, green: 0.42, blue: 0.15))
+              .cornerRadius(15)
+              .foregroundColor(Color(red: 1, green: 0.98, blue: 0.88))
+              .shadow(radius: 10)
+              .padding(2)
+            NavigationLink(destination: homePage()) {
+              Text("Continue as Guest")}
+            .frame(width: 250.0, height: 55.0)
+            .foregroundColor(Color(red: 0.38, green: 0.42, blue: 0.22))
+          }//end of vstack
+          HStack(alignment: .center){
+            Text("--------------")
+            Text("or")
+            Text("--------------")
+          }//end of hstack
+          .padding(.bottom, 30)
+          VStack{
+            Link("Continue with Apple", destination: URL(string: "https://www.apple.com")!)
+          }//end of vstack
+          .frame(width: 250.0, height: 55)
+          .foregroundColor(Color(red: 1, green: 0.98, blue: 0.88))
+          .background(Color(red: 0.38, green: 0.42, blue: 0.22))
+          .cornerRadius(15)
+          .shadow(radius: 8)
+            Link("Continue with Google", destination: URL(string:"https://accounts.google.com/v3/signin/identifier?dsh=S2097275425%3A1687976022955142&ifkv=AeDOFXgsKQHRm1o0O6q8qlI3HW54GoSqbobwMGaQ7T6U7YeWWOSF4Zk5bqlQbEMKn6-91tATouEYog&flowName=GlifWebSignIn&flowEntry=ServiceLogin)")!)
+            .frame(width: 250.0, height: 55.0)
+            .foregroundColor(Color(red: 1, green: 0.98, blue: 0.88))
+            .background(Color(red: 0.38, green: 0.42, blue: 0.22))
+            .cornerRadius(15)
+            .shadow(radius: 8)
+          Button("Continue with Email"){}
+            .frame(width: 250.0, height: 55.0)
+            .foregroundColor(Color(red: 1, green: 0.98, blue: 0.88))
+            .background(Color(red: 0.38, green: 0.42, blue: 0.22))
+            .cornerRadius(15)
+            .shadow(radius: 8)
+            .sheet(isPresented: $showSheet){
+              Text("Enter Name Here:")
+              Button("Submit"){
+                self.showSheet = false
+              }
+              .frame(width: 250.0, height: 55.0)
+              .foregroundColor(Color(red: 1, green: 0.98, blue: 0.88))
+              .background(Color(red: 0.38, green: 0.42, blue: 0.22))
+              .cornerRadius(15)
+              .shadow(radius: 8)
             }
-        
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-struct login_page_Previews: PreviewProvider {
+            .sheet(isPresented: $showSheet2){
+              Text("SiGN UP HERE")
+              Button("Submit"){
+                self.showSheet2 = false
+              }
+              .frame(width: 250.0, height: 55.0)
+              .foregroundColor(Color(red: 1, green: 0.98, blue: 0.88))
+              .background(Color(red: 0.38, green: 0.42, blue: 0.22))
+              .cornerRadius(15)
+              .shadow(radius: 8)
+            }
+        }//end of zsstack
+      //end of nav stack
+    }//end of body
+  }//end of struct
+  struct login_page_Previews: PreviewProvider {
     static var previews: some View {
-        login_page()
+      login_page()
     }
+  }
 }
