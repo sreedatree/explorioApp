c833b4555cb7a96fd379fc351b772c8f0369fe6a
Sreenitya Yalamanchili <sreenitya1@outlook.com>
2023-06-29 10:55:29 -0400

    added views for the navigation bar (nav bar will be added later)

---
+++ explorioApp/views1/ViewThree.swift
@@ -0,0 +1,22 @@
+//
+//  ViewThree.swift
+//  explorioApp
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+struct ViewThree: View {
+    var body: some View {
+        ZStack{
+            Text("this will be the page to redirect back to the search page")
+        }
+    }
+}
+
+struct ViewThree_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewThree()
+    }
+}
---
+++ explorioApp/views1/ViewTwo.swift
@@ -0,0 +1,22 @@
+//
+//  ViewTwo.swift
+//  explorioApp
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+struct ViewTwo: View {
+    var body: some View {
+        ZStack{
+            Text("this will be the page to rerandomize the locations")
+        }
+    }
+}
+
+struct ViewTwo_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewTwo()
+    }
+}
