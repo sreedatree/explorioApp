fa3462ddd4f325fd37b61d56b1c91a1c49b0f92c
Nhi Le <michellelele156@gmail.com>
2023-06-29 10:32:31 -0400

    LocationLandingand2

---
+++ explorioApp/LocationLanding2.swift
@@ -0,0 +1,24 @@
+//
+//  LocationLanding2.swift
+//  explorioApp
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+struct LocationLanding2: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Image("star")
+        }
+    }
+        
+}
+
+struct LocationLanding2_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationLanding2()
+    }
+}
