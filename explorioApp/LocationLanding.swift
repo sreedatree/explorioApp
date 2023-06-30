91c28b8c0a488fa95e25e2bf93d5be9fa81d4645
Nhi Le <michellelele156@gmail.com>
2023-06-30 10:18:32 -0400

    a bunch of stuff

--- explorioApp/LocationLanding.swift
+++ explorioApp/LocationLanding.swift
@@ -22,7 +22,7 @@ struct LocationLanding: View {
     @State private var expanded = true
     var body: some View {
         
-     //   ScrollView{
+            ScrollView{
             NavigationStack {
                 VStack(alignment: .center) {
                     Text("**Welcome to**")
@@ -115,7 +115,7 @@ struct LocationLanding: View {
                     }
                 }
             }
-        //}
+        }
     }
     
 }
